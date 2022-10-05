52e74705482da50a65e4971886c340189ce15b4e
saif-ulhussain <saif-ul.hussain@sainsburys.co.uk>
2022-10-05 10:21:22 +0100

    Refactored code, removed multiple classes into single Calculate class

---
+++ Calculate.cs
@@ -0,0 +1,46 @@
+using System;
+namespace Calculator.App
+{
+    public class Calculate
+    {
+        public int Add(int num1, int num2)
+        {
+            return num1 + num2;
+        }
+
+        public int Subtract(int num1, int num2)
+        {
+            return num1 - num2;
+        }
+
+        public int Multiply(int num1, int num2)
+        {
+            int result = 0;
+            for (int i = 0; i < num2; i++)
+            {
+                result += num1;
+            }
+            return result;
+        }
+
+        public int Divide(int num1, int num2)
+        {
+            int result = 0;
+            int number = num1;
+            while (number >= 0)
+            {
+                if ((number - num2) >= 0)
+                {
+                    result++;
+                    number -= num2;
+                }
+                else
+                {
+                    break;
+                }
+            }
+            return result;
+        }
+    }
+}
+
--- Program.cs
+++ Program.cs
@@ -1,2 +1,37 @@
-﻿// See https://aka.ms/new-console-template for more information
-Console.WriteLine("Hello, World!");
+﻿using System.Linq.Expressions;
+using Calculator.App;
+namespace InitialTraining.Calculator.App
+{
+    static class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Please enter operation followed by two numbers.");
+
+            string[] response = Console.ReadLine().Split(" ");
+
+            string operation = response[0];
+            int num1 = Int32.Parse(response[1]);
+            int num2 = Int32.Parse(response[2]);
+
+
+            Calculate calculate = new Calculate();
+
+            switch (operation)
+            {
+                case "add":
+                    Console.WriteLine(calculate.Add(num1, num2));
+                    break;
+                case "subtract":
+                    Console.WriteLine(calculate.Subtract(num1, num2));
+                    break;
+                case "multiply":
+                    Console.WriteLine(calculate.Multiply(num1, num2));
+                    break;
+                case "divide":
+                    Console.WriteLine(calculate.Divide(num1, num2));
+                    break;
+            }
+        }
+    }
+}
